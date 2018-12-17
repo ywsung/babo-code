995c2800abd525ab1230962b1042551fbf36f190
babo <seirion@mensakorea.org>
2018-12-17 20:55:32 +0900

    add Gson

---
+++ app/src/main/java/com/seirion/code/util/IoUtils.kt
@@ -0,0 +1,16 @@
+package com.seirion.code.util
+
+import com.seirion.code.db.CodeData
+import java.util.*
+
+class IoUtils {
+    companion object {
+        fun toJson(codeData: List<CodeData>): String {
+            return ""
+        }
+
+        fun fromJson(json: String): List<CodeData> {
+            return Collections.emptyList()
+        }
+    }
+}
