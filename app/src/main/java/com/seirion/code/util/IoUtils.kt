600e4bd579fb169a9cff74bab1bfcb1edec06372
babo <seirion@mensakorea.org>
2018-12-18 10:45:27 +0900

    implement json conversion

--- app/src/main/java/com/seirion/code/util/IoUtils.kt
+++ app/src/main/java/com/seirion/code/util/IoUtils.kt
@@ -1,16 +1,25 @@
 package com.seirion.code.util
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonSyntaxException
 import com.seirion.code.db.CodeData
 import java.util.*
+
+
 
 class IoUtils {
     companion object {
         fun toJson(codeData: List<CodeData>): String {
-            return ""
+            var gson = Gson()
+            return gson.toJson(codeData)
         }
 
-        fun fromJson(json: String): List<CodeData> {
-            return Collections.emptyList()
+        fun fromJson(json: String): List<CodeData> = try {
+            val gson = GsonBuilder().create()
+            gson.fromJson(json, Array<CodeData>::class.java).toList()
+        } catch (e: JsonSyntaxException) {
+            Collections.emptyList()
         }
     }
 }
